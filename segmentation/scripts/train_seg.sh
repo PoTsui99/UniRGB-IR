d058487f53f6cb9280e1c0a4597f2171c2e704bb
Po Tsui <46322883+PoTsui99@users.noreply.github.com>
2025-06-17 02:03:56 +0800

    Ummm.. I forgot the params needed.

--- segmentation/scripts/train_seg.sh
+++ segmentation/scripts/train_seg.sh
@@ -4,5 +4,5 @@ source activate $ENV_NAME || conda activate $ENV_NAME
 
 CUDA_HOME=/usr/local/cuda-11.4 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
 tools/dist_train.sh \
-configs/_vpt_rgbt_segmentation/MFNet/SETR/mla_v15_mae_coco_2xb2_80k_withBG.py \
+configs/_vpt_rgbt_segmentation/MFNet/SETR/mla_v15_mae_coco_2xb2_80k_withBG.py 2 \
 --work-dir work_dirs/mfnet_mla_v15_mae_coco_2xb2_80k_withBG
